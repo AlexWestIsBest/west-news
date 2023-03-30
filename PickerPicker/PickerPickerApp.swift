686799805ee4880390b80415aba4dd8d6dee12a3
Alex Westerlund <alexwesterlund@outlook.com>
2023-03-30 17:04:07 -0400

    Initial Commit

---
+++ PickerPicker/PickerPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerPickerApp.swift
+//  PickerPicker
+//
+//  Created by Alex Westerlund on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PickerPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
